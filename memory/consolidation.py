ae534d1b5494f22ee4ce6ed6fb6b8a8c3f9a2825
Meta_Sama <sagda4sherif@gmail.com>
2026-08-08 02:47:22 +0300

    Update consolidation.py

--- memory/consolidation.py
+++ memory/consolidation.py
@@ -1,1 +1,24 @@
+class ConsolidationLayer:
+    def __init__(self, episodic_memory, semantic_memory):
+        self.episodic_memory = episodic_memory
+        self.semantic_memory = semantic_memory
 
+    def consolidate(self):
+        promoted = 0
+
+        for episode in self.episodic_memory.get_all():
+            metadata = episode.get("metadata", {})
+            key = metadata.get("key")
+
+            if not key:
+                continue
+
+            self.semantic_memory.store(
+                key,
+                episode["event"],
+                metadata
+            )
+
+            promoted += 1
+
+        return promoted
